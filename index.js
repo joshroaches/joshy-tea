1cf2e058cd8939b02c3b2afbbd4693de21159d88
joshroaches <149947889+joshroaches@users.noreply.github.com>
2024-03-04 21:06:55 +0000

    Create handlers

---
+++ index.js
@@ -0,0 +1,43 @@
+class JoshyTea {
+    constructor() {
+      this.teaData = {};
+    }
+  
+    setTeaData(key, value) {
+      this.teaData[key] = value;
+      console.log(`Set Joshy Tea data for key '${key}':`, value);
+    }
+  
+    getTeaData(key) {
+      const value = this.teaData[key];
+      console.log(`Retrieved Joshy Tea data for key '${key}':`, value);
+      return value;
+    }
+  
+    brewTea() {
+      console.log('Brewing a delightful cup of Joshy Tea. ☕🌈');
+    }
+  
+    customizeBlend(blend) {
+      console.log(`Customizing Joshy Tea blend with ${blend}.`);
+    }
+  
+    addJoyfulIngredient(ingredient) {
+      console.log(`Adding a joyful touch of ${ingredient} to elevate the spirit of Joshy Tea.`);
+    }
+  
+    enjoyTea() {
+      console.log('Sipping on the joyful flavor of Joshy Tea. 😊🍵');
+    }
+  }
+  
+  // Example usage:
+  const joshyTea = new JoshyTea();
+  
+  joshyTea.setTeaData('flavor', 'Joyful Fusion');
+  joshyTea.setTeaData('aroma', 'Elevating Essence');
+  joshyTea.brewTea();
+  joshyTea.customizeBlend('Sunny Citrus');
+  joshyTea.addJoyfulIngredient('Happiness Drops');
+  joshyTea.enjoyTea();
+  
